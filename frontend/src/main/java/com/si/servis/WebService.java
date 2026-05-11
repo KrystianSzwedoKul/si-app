bf63f3ac01a0ba9f922fa29de40f84f1f49a4b0b
Szwedo Krystian <krystian.szwedo@kul.pl>
2026-05-11 16:36:02 +0200

    lab 11

--- frontend/src/main/java/com/si/servis/WebService.java
+++ frontend/src/main/java/com/si/servis/WebService.java
@@ -1,10 +1,10 @@
 package com.si.servis;
 
 import com.nimbusds.jose.shaded.gson.Gson;
-import com.si.config.WebClientConfig;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
@@ -16,7 +16,7 @@ public class WebService {
         this.config = config;
     }
 
-    public Map<String,String> isValidSentence(String language, String sentence) {
+    public Map<String, String> isValidSentence(String language, String sentence) {
 
         String result = config.get()
                 .uri("/api/v1/gemini/" + language + "/" + sentence)
@@ -24,7 +24,22 @@ public class WebService {
                 .bodyToFlux(String.class)
                 .blockFirst();
         Gson gson = new Gson();
-        Map<String,String> map = gson.fromJson(result, Map.class);
+        Map<String, String> map = gson.fromJson(result, Map.class);
         return map;
+    }
+
+    public boolean isValidNumber(String number, byte[] image) {
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("number", number);
+        body.put("image", image);
+
+        Boolean result = config.post()
+                .uri("/api/v1/gemini/")
+                .bodyValue(body)
+                .retrieve()
+                .bodyToFlux(Boolean.class)
+                .blockFirst();
+        return result;
     }
 }
